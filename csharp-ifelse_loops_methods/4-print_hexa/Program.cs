27299953817b647d235a05442713e39b049bcb7b
Stephen30o0 <o.olurinola@alustudent.com>
2023-09-23 16:51:09 -0700

    hexa

---
+++ csharp-ifelse_loops_methods/4-print_hexa/Program.cs
@@ -0,0 +1,9 @@
+﻿﻿using System;
+
+class Program{
+    static void Main(String[] args){
+         for(int hexa = 0; hexa < 99 ; hexa++){
+            Console.WriteLine($"{hexa} = 0x{hexa:X}");
+         }
+    }
+}

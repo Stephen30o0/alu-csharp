b797b2c16f6224c478def2471e20039c74cf336e
Stephen30o0 <o.olurinola@alustudent.com>
2023-09-23 13:42:45 -0700

    currency

---
+++ csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -0,0 +1,12 @@
+﻿﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		double percent = .7553;
+		double currency = 98765.4321;
+		Console.WriteLine($"{Environment.NewLine}Percent: {percent * 100}");
+        Console.WriteLine($"{Environment.NewLine}Currency:{string.Format("{0:C}", currency) }");
+	}
+}

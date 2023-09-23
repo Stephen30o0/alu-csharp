32f1ca038781d5ea2aad8902bc11363e99cbfe10
Stephen30o0 <o.olurinola@alustudent.com>
2023-09-23 12:51:26 -0700

    street

--- csharp-hello_world/7-concat/7-concat.cs
+++ csharp-hello_world/7-concat/7-concat.cs
@@ -5,7 +5,7 @@ class Program
 	static void Main(string[] args)
 	{
 		string str1 = "Holberton";
-		string str2 = " School";
+		string str2 = " Street";
 		string str3 = str1 + str2;
 		Console.WriteLine($"Welcome to {str3}!");
 	}

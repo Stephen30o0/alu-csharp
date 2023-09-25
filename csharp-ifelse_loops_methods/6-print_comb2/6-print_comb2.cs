1c63cc2825f10f40058630a44ac5274e64a7bf1c
Stephen30o0 <o.olurinola@alustudent.com>
2023-09-25 06:04:34 -0700

    firstlast

---
+++ csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int firstDigit = 0; firstDigit <= 8; firstDigit++)
+        {
+            for (int secondDigit = firstDigit + 1; secondDigit <= 9; secondDigit++)
+            {
+                Console.Write($"{firstDigit:D}{secondDigit:D}{(firstDigit < 8 || secondDigit < 9 ? ", " : "\n")}");
+            }
+        }
+    }
+}

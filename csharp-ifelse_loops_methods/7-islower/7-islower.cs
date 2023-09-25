6fe35d9a5cb281cd4070ee3307bec266efea0d15
Stephen30o0 <o.olurinola@alustudent.com>
2023-09-25 06:23:57 -0700

    lower

---
+++ csharp-ifelse_loops_methods/7-islower/7-islower.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Character
+{
+    public static bool IsLower(char c)
+    {
+        return c >= 'a' && c <= 'z';
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a sequence of characters:");
+
+        string input = Console.ReadLine();
+
+        foreach (char c in input)
+        {
+            Console.WriteLine($"{c} is {(IsLower(c) ? "lowercase" : "uppercase")}");
+        }
+    }
+}

c13d4f271cee7f7eaffef0adff0b92fdea2fffe4
Stephen30o0 <o.olurinola@alustudent.com>
2023-09-25 08:51:10 -0700

    add

---
+++ csharp-ifelse_loops_methods/9-add/9-add.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Number
+{
+    public static int Add(int a, int b)
+    {
+       int result = a + b;
+        return result;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("{0}", Number.Add(1, 2));
+        Console.WriteLine("{0}", Number.Add(98, 0));
+        Console.WriteLine("{0}", Number.Add(100, -2));
+    }
+}

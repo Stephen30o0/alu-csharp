a066d76553426b6aa9f9d19dc0b2fc06ecd94407
Stephen30o0 <o.olurinola@alustudent.com>
2023-09-25 08:44:58 -0700

    lastdig

---
+++ csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Number
+{
+    public static int PrintLastDigit(int number)
+    {
+        number = Math.Abs(number);
+
+        int lastDigit = number % 10;
+
+        Console.WriteLine($"Last digit of {number} is {lastDigit}");
+
+        return lastDigit;
+    }
+
+    public static void Main(string[] args)
+    {
+        int num = 12345;
+        int lastDigit = PrintLastDigit(num);
+    }
+}

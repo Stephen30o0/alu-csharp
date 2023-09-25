5860f5163ef78add46502f108c450ca81bfcb31c
Stephen30o0 <o.olurinola@alustudent.com>
2023-09-25 09:09:12 -0700

    line

---
+++ csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintLine(int length)
+    {
+        if (length <= 0)
+        {
+            Console.WriteLine();
+        }
+        else
+        {
+            string line = new string('_', length);
+            Console.WriteLine(line);
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Line.PrintLine(3);
+        Line.PrintLine(0);
+        Line.PrintLine(12);
+        Line.PrintLine(-98);
+    }
+}

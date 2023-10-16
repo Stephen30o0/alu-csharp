ba9432fd134a4aff3f17dcf5b47752881fe9885b
Stephen30o0 <o.olurinola@alustudent.com>
2023-10-16 16:00:19 -0700

    n

--- csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
+++ csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -4,14 +4,10 @@ public class Array
 {
     public static void Reverse(int[] array)
     {
-        if (array == null)
-        {
-            return;
-        }
-
         for (int i = array.Length - 1; i >= 0; i--)
         {
             Console.Write(array[i] + " ");
         }
+        Console.WriteLine();
     }
 }

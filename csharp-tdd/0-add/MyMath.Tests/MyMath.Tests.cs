0910687d724140d510b2ce07a16078cb3443f37a
Stephen30o0 <o.olurinola@alustudent.com>
2023-10-20 08:47:46 -0700

    test

---
+++ csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyMath;
+
+namespace MyMath.Tests
+{
+    [TestClass]
+    public class OperationsTests
+    {
+        [TestMethod]
+        public void TestAdd()
+        {
+            // Arrange
+            int a = 2;
+            int b = 3;
+
+            // Act
+            int result = Operations.Add(a, b);
+
+            // Assert
+            Assert.AreEqual(5, result);
+        }
+    }
+}

74a381c9de4a185851238160a1c5a074c1f78cf1
Stephen30o0 <o.olurinola@alustudent.com>
2023-11-13 06:57:45 -0800

    bits

--- csharp-inheritance/0-is/0-is.cs
+++ csharp-inheritance/0-is/0-is.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿﻿using System;
+
+public class Obj
+{
+    public static bool IsOfTypeInt(object obj)
+    {
+        return obj is int;
+    }
+}

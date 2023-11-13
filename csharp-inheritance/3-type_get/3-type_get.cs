253f14c80874766bba7dcfc7357ce7371698da9b
Stephen30o0 <o.olurinola@alustudent.com>
2023-11-13 07:18:44 -0800

    tn

--- csharp-inheritance/3-type_get/3-type_get.cs
+++ csharp-inheritance/3-type_get/3-type_get.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Reflection;
+
+public class Obj
+{
+    public static void Print(object myObj)
+    {
+        Type objectType = myObj.GetType();
+
+        Console.WriteLine($"Properties and Methods of {objectType.Name}:");
+
+        // Print Properties
+        Console.WriteLine("Properties:");
+        foreach (var property in objectType.GetProperties())
+        {
+            Console.WriteLine($"- {property.Name}");
+        }
+
+        // Print Methods
+        Console.WriteLine("\nMethods:");
+        foreach (var method in objectType.GetMethods())
+        {
+            Console.WriteLine($"- {method.Name}");
+        }
+    }
+}

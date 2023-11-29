ae23827ec9d50757d5e6b19ebb1710e9de4b7999
Stephen30o0 <o.olurinola@alustudent.com>
2023-11-29 12:46:21 +0200

    cop

--- csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -1,38 +1,18 @@
-﻿using System;
+﻿﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
-public class VectorMath
-{
-    public static double Magnitude(double[] vector)
-    {
-        int dimensions = vector.Length;
-
-        if (dimensions != 2 && dimensions != 3)
-        {
-            return -1; // Return -1 for vectors that are not 2D or 3D
+class VectorMath{
+    public static double Magnitude(double[] vector){
+    
+        if(vector.Length == 2){
+            return Math.Round(Math.Sqrt( Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2)), 2);
         }
 
-        double sumOfSquares = 0;
-        foreach (double component in vector)
-        {
-            sumOfSquares += Math.Pow(component, 2);
+        if(vector.Length == 3){
+             return Math.Round(Math.Sqrt(Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2) + Math.Pow(vector[2], 2)), 2);
         }
 
-        double magnitude = Math.Sqrt(sumOfSquares);
-        // Round the magnitude to the nearest hundredth
-        magnitude = Math.Round(magnitude, 2);
-
-        return magnitude;
-    }
-
-    // Example usage
-    public static void Main(string[] args)
-    {
-        double[] vector2D = { 3.0, 4.0 }; // Example 2D vector
-        double[] vector3D = { 1.0, 2.0, 3.0 }; // Example 3D vector
-        double[] invalidVector = { 1.0, 2.0, 3.0, 4.0 }; // Example invalid vector
-
-        Console.WriteLine("Magnitude of 2D vector: " + Magnitude(vector2D));
-        Console.WriteLine("Magnitude of 3D vector: " + Magnitude(vector3D));
-        Console.WriteLine("Magnitude of invalid vector: " + Magnitude(invalidVector));
+        return -1;
     }
 }

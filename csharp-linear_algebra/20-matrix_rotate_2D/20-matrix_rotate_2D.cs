f65b4bcb3860568dceb0e1044e0f1b72818d491c
Stephen30o0 <o.olurinola@alustudent.com>
2023-11-29 14:05:50 +0200

    fix

--- csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
+++ csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
@@ -1,59 +1,49 @@
-﻿using System;
+﻿﻿using System;
 
-public class MatrixMath
-{
-    public static double[,] Rotate2D(double[,] matrix, double angle)
-    {
+
+class Program{
+    
+    public static void Main(string[] arg){
+        
+        double[,] TestMatrix = { { 1, 2}, {3, 4}};
+
+        double[,] resultMatrix = MatrixMath.Rotate2D(TestMatrix, 1);
+        
+        Console.WriteLine($"{resultMatrix[0,0]}  --  {resultMatrix[0,1]}\n{resultMatrix[1,0]}  --  {resultMatrix[1,1]}");
+
+    }
+}
+class MatrixMath{
+    public static double[,] Rotate2D(double[,] matrix, double angle){
+        double[,] rotationMatrix = {
+            { Math.Cos(angle), Math.Sin(angle)},
+            { -(Math.Sin(angle)), Math.Cos(angle)}
+        };
+
         int rows = matrix.GetLength(0);
         int cols = matrix.GetLength(1);
 
-        if (rows != cols || rows < 2)
-        {
-            // Return a matrix containing -1 if the matrix is not a valid square 2D matrix
-            return new double[,] { { -1 } };
-        }
+        if(rows == cols && rows == 2 && cols == 2){
+         
+         double[,] result = new double[rows,rows];
 
-        double cosAngle = Math.Cos(angle);
-        double sinAngle = Math.Sin(angle);
-
-        double[,] result = new double[rows, cols];
-        for (int i = 0; i < rows; i++)
-        {
-            for (int j = 0; j < cols; j++)
+         for (int col = 0; col < cols; col++)
             {
-                double x = i - rows / 2.0;
-                double y = j - cols / 2.0;
-
-                // Apply rotation transformation
-                double newX = x * cosAngle - y * sinAngle + rows / 2.0;
-                double newY = x * sinAngle + y * cosAngle + cols / 2.0;
-
-                // Interpolate the rotated value from the original matrix
-                result[i, j] = Interpolate(matrix, newX, newY);
+                for (int row = 0; row < rows; row++)
+                {
+                    result[row, col] = 0;
+                    for (int k = 0; k < rows; k++)
+                    {
+                        result[row, col] += Math.Round( rotationMatrix[row, k] * matrix[k, col] , 2);
+                    }
+                }
             }
+
+            return result;
         }
 
-        return result;
+        return new double[,]{{-1}};
+       
     }
-
-    private static double Interpolate(double[,] matrix, double x, double y)
-    {
-        int rows = matrix.GetLength(0);
-        int cols = matrix.GetLength(1);
-
-        int x1 = (int)Math.Floor(x);
-        int x2 = Math.Min(x1 + 1, rows - 1);
-        int y1 = (int)Math.Floor(y);
-        int y2 = Math.Min(y1 + 1, cols - 1);
-
-        double q11 = matrix[x1, y1];
-        double q21 = matrix[x2, y1];
-        double q12 = matrix[x1, y2];
-        double q22 = matrix[x2, y2];
-
-        double value = ((x2 - x) * (y2 - y) * q11 + (x - x1) * (y2 - y) * q21 +
-                        (x2 - x) * (y - y1) * q12 + (x - x1) * (y - y1) * q22);
-
-        return value;
-    }
+  
 }

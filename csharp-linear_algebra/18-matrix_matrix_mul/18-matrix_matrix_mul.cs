d68dcf5d7487d6c4e873c765de2fa8aec49cef8e
Stephen30o0 <o.olurinola@alustudent.com>
2023-11-29 13:56:42 +0200

    dre

--- csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
+++ csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
@@ -1,33 +1,30 @@
-﻿using System;
+﻿﻿using System;
 
-public class MatrixMath
-{
-    public static double[,] Multiply(double[,] matrix1, double[,] matrix2)
-    {
-        int rows1 = matrix1.GetLength(0);
-        int cols1 = matrix1.GetLength(1);
-        int rows2 = matrix2.GetLength(0);
-        int cols2 = matrix2.GetLength(1);
+class MatrixMath{
 
-        if (cols1 != rows2 || (rows1 != 2 && rows1 != 3) || (cols1 != 2 && cols1 != 3) || (rows2 != 2 && rows2 != 3) || (cols2 != 2 && cols2 != 3))
-        {
-            // Return a matrix containing -1 if matrices cannot be multiplied
-            return new double[,] { { -1 } };
-        }
+    public static double[,] Multiply(double[,] matrix1, double[,] matrix2){
 
-        double[,] result = new double[rows1, cols2];
-        for (int i = 0; i < rows1; i++)
-        {
-            for (int j = 0; j < cols2; j++)
-            {
-                result[i, j] = 0;
-                for (int k = 0; k < cols1; k++)
-                {
-                    result[i, j] += matrix1[i, k] * matrix2[k, j];
+        int rowA = matrix1.GetLength(0);
+        int colA = matrix1.GetLength(1);
+        int rowB = matrix2.GetLength(0);
+        int colB = matrix2.GetLength(1);
+
+        double[,] result = new double[rowA, colB];
+       
+        if(colA == rowB){
+            for(int i = 0; i < rowA; i++){
+
+                for(int j = 0; j < colB; j++){
+                  
+                  for(int u = 0; u < colA; u++){
+                        result[i,j] += matrix1[i, u] * matrix2[u, j];
+                  }
                 }
             }
+
+            return result;
         }
 
-        return result;
+        return new double[,]{{-1}};
     }
 }

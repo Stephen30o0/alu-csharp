2e80062494d0ec793ef971da49621000e3d2ecc0
Stephen30o0 <o.olurinola@alustudent.com>
2023-11-29 13:23:55 +0200

    xx

---
+++ csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class VectorMath
+{
+    public static double[] Multiply(double[] vector, double scalar)
+    {
+        int dimensions = vector.Length;
+
+        if (dimensions != 2 && dimensions != 3)
+        {
+            // Return a vector containing -1 if the vector is not 2D or 3D
+            return new double[] { -1 };
+        }
+
+        double[] result = new double[dimensions];
+        for (int i = 0; i < dimensions; i++)
+        {
+            result[i] = vector[i] * scalar;
+        }
+
+        return result;
+    }
+}

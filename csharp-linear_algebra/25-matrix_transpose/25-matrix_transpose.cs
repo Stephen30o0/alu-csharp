5df168427e3ddab8b84abdb4822bd01f15f91b47
Stephen30o0 <o.olurinola@alustudent.com>
2023-11-29 14:33:07 +0200

    houston

---
+++ csharp-linear_algebra/25-matrix_transpose/25-matrix_transpose.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class MatrixMath
+{
+    public static double[,] Transpose(double[,] matrix)
+    {
+        int rows = matrix.GetLength(0);
+        int cols = matrix.GetLength(1);
+
+        if (rows == 0 || cols == 0)
+        {
+            // Return an empty matrix if the input matrix is empty
+            return new double[,] { };
+        }
+
+        double[,] result = new double[cols, rows];
+
+        for (int i = 0; i < cols; i++)
+        {
+            for (int j = 0; j < rows; j++)
+            {
+                result[i, j] = matrix[j, i];
+            }
+        }
+
+        return result;
+    }
+}

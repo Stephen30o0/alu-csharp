2f82c34aba6a78337bfcda0448fc37fe6912f3d7
Gatwaza <gatwazarobert177@gmail.com>
2024-10-19 17:03:56 +0200

    O

---
+++ csharp-generics/0-queue/0-queue.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Queue<T>
+{
+    public Queue()
+    {
+    }
+
+    // Method to return the type of the queue
+    public Type CheckType()
+    {
+        return typeof(T);
+    }
+}

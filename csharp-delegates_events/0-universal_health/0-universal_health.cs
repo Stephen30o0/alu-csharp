3f953a1e049b5d980ad01f504b9fcba8d6f1cdc0
Gatwaza <gatwazarobert177@gmail.com>
2024-10-25 16:12:25 +0200

    jh

--- csharp-delegates_events/0-universal_health/0-universal_health.cs
+++ csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 
+/// <summary>
+/// Represents a player with health attributes.
+/// </summary>
 public class Player
 {
     // Private properties
@@ -7,7 +10,11 @@ public class Player
     private float maxHp;
     private float hp;
 
-    // Constructor
+    /// <summary>
+    /// Initializes a new instance of the Player class.
+    /// </summary>
+    /// <param name="name">The name of the player (default is "Player").</param>
+    /// <param name="maxHp">The maximum health points of the player (default is 100).</param>
     public Player(string name = "Player", float maxHp = 100f)
     {
         if (maxHp <= 0)
@@ -24,7 +31,9 @@ public class Player
         this.name = name;
     }
 
-    // Method to print health
+    /// <summary>
+    /// Prints the current health status of the player.
+    /// </summary>
     public void PrintHealth()
     {
         Console.WriteLine($"{name} has {hp} / {maxHp} health");

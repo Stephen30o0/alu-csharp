8997308b6e025217d8f001f6eb0785a02029fff4
Gatwaza <gatwazarobert177@gmail.com>
2024-10-25 14:26:17 +0200

    LK

---
+++ csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public abstract class Base
+{
+    // Public property 'name' of type string
+    public string name { get; set; }
+
+    // Override ToString() method
+    public override string ToString()
+    {
+        return $"{name} is a {GetType().Name}";
+    }
+}
+
+class SoftwareEngineer : Base
+{
+    // Empty class as per the requirements
+}

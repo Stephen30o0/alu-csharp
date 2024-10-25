ef57a58dd6f449a081ed9b4883dd35214fa474f8
Gatwaza <gatwazarobert177@gmail.com>
2024-10-25 16:04:14 +0200

    0

---
+++ csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class Player
+{
+    // Private properties
+    private string name;
+    private float maxHp;
+    private float hp;
+
+    // Constructor
+    public Player(string name = "Player", float maxHp = 100f)
+    {
+        if (maxHp <= 0)
+        {
+            this.maxHp = 100f;
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+        }
+        else
+        {
+            this.maxHp = maxHp;
+        }
+
+        this.hp = this.maxHp;
+        this.name = name;
+    }
+
+    // Method to print health
+    public void PrintHealth()
+    {
+        Console.WriteLine($"{name} has {hp} / {maxHp} health");
+    }
+}

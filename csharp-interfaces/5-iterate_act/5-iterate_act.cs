d43f9900126e08af62e36f93910106a6b03542e6
Gatwaza <gatwazarobert177@gmail.com>
2024-10-25 15:35:36 +0200

    kj

--- csharp-interfaces/5-iterate_act/5-iterate_act.cs
+++ csharp-interfaces/5-iterate_act/5-iterate_act.cs
@@ -1,13 +1,129 @@
 ﻿using System;
 using System.Collections.Generic;
 
+// Base class providing foundational properties for inheritance
+abstract class Base
+{
+    private string _name = string.Empty; // Backing field for name
+
+    // Property to get or set the name associated with the instance
+    public string name
+    {
+        get => _name;
+        set => _name = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    // Overrides the default ToString() method to display custom properties
+    public override string ToString()
+    {
+        return $"{name} is a {this.GetType().Name}";
+    }
+}
+
+// Interfaces
+public interface IInteractive
+{
+    void Interact();
+}
+
+public interface IBreakable
+{
+    int durability { get; set; }
+    void Break();
+}
+
+public interface ICollectable
+{
+    bool isCollected { get; set; }
+    void Collect();
+}
+
+// Door class implementing IInteractive
+class Door : Base, IInteractive
+{
+    public Door(string name = "Door")
+    {
+        this.name = name;
+    }
+
+    public void Interact()
+    {
+        Console.WriteLine($"You try to open the {name}. It's locked.");
+    }
+}
+
+// Decoration class implementing IInteractive and IBreakable
+class Decoration : Base, IInteractive, IBreakable
+{
+    public int durability { get; set; }
+    public bool isQuestItem { get; set; }
+
+    public Decoration(string name = "Decoration", int durability = 1, bool isQuestItem = false)
+    {
+        this.name = name;
+
+        if (durability <= 0)
+            throw new ArgumentException("Durability must be greater than 0");
+        this.durability = durability;
+        this.isQuestItem = isQuestItem;
+    }
+
+    public void Interact()
+    {
+        if (durability <= 0)
+            Console.WriteLine($"The {name} has been broken.");
+        else if (isQuestItem)
+            Console.WriteLine($"You look at the {name}. There's a key inside.");
+        else
+            Console.WriteLine($"You look at the {name}. Not much to see here.");
+    }
+
+    public void Break()
+    {
+        if (durability > 0)
+        {
+            durability--;
+            if (durability > 0)
+                Console.WriteLine($"You hit the {name}. It cracks.");
+            else
+                Console.WriteLine($"You smash the {name}. What a mess.");
+        }
+        else
+        {
+            Console.WriteLine($"The {name} is already broken.");
+        }
+    }
+}
+
+// Key class implementing ICollectable
+class Key : Base, ICollectable
+{
+    public bool isCollected { get; set; }
+
+    public Key(string name = "Key", bool isCollected = false)
+    {
+        this.name = name;
+        this.isCollected = isCollected;
+    }
+
+    public void Collect()
+    {
+        if (!isCollected)
+        {
+            isCollected = true;
+            Console.WriteLine($"You pick up the {name}.");
+        }
+        else
+        {
+            Console.WriteLine($"You have already picked up the {name}.");
+        }
+    }
+}
+
+// RoomObjects class with IterateAction method
 class RoomObjects
 {
-    /// <summary>
-    /// Iterates through a list of room objects and executes methods based on their implemented interfaces.
-    /// </summary>
-    /// <param name="roomObjects">A list of objects to iterate over.</param>
-    /// <param name="type">The type of interface to check for.</param>
+    // Iterates through a list of room objects and executes methods based on their implemented interfaces
     public static void IterateAction(List<Base> roomObjects, Type type)
     {
         foreach (var obj in roomObjects)
@@ -28,5 +144,30 @@ class RoomObjects
                 collectable.Collect();
             }
         }
+    }
+}
+
+// Main program
+class Program
+{
+    static void Main(string[] args)
+    {
+        Door frontDoor = new Door("Front Door");
+        Decoration photo = new Decoration("Awkward Family Portrait");
+        Decoration antiqueVase = new Decoration("Antique Vase", 2, true);
+        Key carKey = new Key("Car Key");
+
+        List<Base> allObjects = new List<Base>
+        {
+            frontDoor,
+            photo,
+            antiqueVase,
+            carKey
+        };
+
+        // Execute actions based on interfaces
+        RoomObjects.IterateAction(allObjects, typeof(IInteractive));
+        RoomObjects.IterateAction(allObjects, typeof(IBreakable));
+        RoomObjects.IterateAction(allObjects, typeof(ICollectable));
     }
 }
